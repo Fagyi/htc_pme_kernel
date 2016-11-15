2d4b535888d7261e85ef49d76167f66df6816ba9
Viresh Kumar <viresh.kumar@linaro.org>
2016-11-15 13:53:21 +0530

    cpufreq: schedutil: enable fast switch earlier
    The fast_switch_enabled flag will be used by both sugov_policy_alloc()
    and sugov_policy_free() with a later patch.
    
    Prepare for that by moving the calls to enable and disable it to the
    beginning of sugov_init() and end of sugov_exit().
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -759,6 +759,7 @@ static int sugov_exit(struct cpufreq_policy *policy)
 	
 	sugov_kthread_stop(sg_policy);
 	sugov_policy_free(sg_policy);
+	cpufreq_disable_fast_switch(policy);
 
 	cpufreq_disable_fast_switch(policy);
 	return 0;
