7107d68682cc9cb8db949b4a333a12f3ba1a47c2
Maulik Shah <mkshah@codeaurora.org>
2017-08-26 20:32:32 +0100

    drivers: cpuidle: Minimize round off errors in wake up time
    Current implementation adds more delay in wake up time programmed
    due to round off errors. Minimize round off errors in calculation.
    
    Change-Id: Iddb19c6dae79c999de9fae444f0c64aac0500f42
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -656,6 +656,8 @@ static int cluster_configure(struct lpm_cluster *cluster, int idx,
 	if (level->notify_rpm) {
 		struct cpumask nextcpu, *cpumask;
 		uint64_t us;
+		uint64_t sec;
+		uint64_t nsec;
 
 		us = get_cluster_sleep_time(cluster, &nextcpu, from_idle);
 		cpumask = level->disable_dynamic_routing ? NULL : &nextcpu;
@@ -666,8 +668,17 @@ static int cluster_configure(struct lpm_cluster *cluster, int idx,
 			goto failed_set_mode;
 		}
 
-		us = (us + 1) * 1000;
-		do_div(us, NSEC_PER_SEC/SCLK_HZ);
+		us = us + 1;
+		sec = us;
+		do_div(sec, USEC_PER_SEC);
+		nsec = us - sec * USEC_PER_SEC;
+
+		sec = sec * SCLK_HZ;
+		if (nsec > 0) {
+			nsec = nsec * NSEC_PER_USEC;
+			do_div(nsec, NSEC_PER_SEC/SCLK_HZ);
+		}
+		us = sec + nsec;
 		msm_mpm_enter_sleep(us, from_idle, cpumask);
 
 		if (cluster->no_saw_devices && !use_psci)
